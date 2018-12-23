46862eafdc558bebb0a39690225ae6f70de59c52
boldizsarf <fodorboldi7@gmail.com>
2018-12-23 23:23:23 +0100

    Version 1.0
    You can add URL rewrites easily, with a configuration file. This engine supports Apache2, and IIS.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Quick Rewrite Engine
+ * by Boldizsar Fodor
+ * Version 0.1
+ */
+
+/** Load the config */
+$config = simplexml_load_file('config.xml');
+
+/** Explode params */
+$param = explode('/', $_SERVER['REQUEST_URI']);
+
+
+/** Index */
+if (empty($param[1])) {
+    if (isset($config->index['href'])) {
+        require $config->index['href'];
+    }
+}
+
+/** Pages az params */
+foreach ($config->page as $page) {
+    if ($param[1] == $page['name']) {
+        foreach ($page->param as $paramcf) {
+            $name = $paramcf["string_name"];
+            $$name = $param[intval($paramcf["id"])];
+        }
+        require $page['href'];
+    }
+}
